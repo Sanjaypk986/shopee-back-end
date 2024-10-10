5d11ad4647bb85413d23a8d49d914b81036f49c6
swafvanSFK <safuswafvan1@gmail.com>
2024-10-10 19:43:59 +0530

    Created cart controller and make same changes in cart model

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,33 +1,22 @@
 const mongoose = require('mongoose');
 
-const productItemSchema = mongoose.Schema(
-  {
-    productId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product', // Reference to the Product model
-      required: true,
+// Define the Cart Schema containing an array of products
+const cartSchema = new mongoose.Schema({
+  products: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product', // Reference to the Product model
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
     },
-    quantity: {
-      type: Number,
-      required: true,
-      default: 1, // Default to 1 unit
-    },
-    totalPrice: {
-      type: Number,
-      required: true,
-      default: 0, // Calculate based on quantity * price
-    },
-  },
-);
+  ],
+});
 
-// Main product schema containing the global products array
-const globalProductsSchema = mongoose.Schema(
-  {
-    products: [productItemSchema], // Array of product items
-  },
-);
-
-// Export the model to be used in the app
-const GlobalProducts = mongoose.model('GlobalProducts', globalProductsSchema);
-
-module.exports = GlobalProducts;
+const Cart = mongoose.model('Cart', cartSchema);
+module.exports = Cart;
