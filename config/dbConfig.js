2f8423148d1b7d33c07ec8e99aff2dbc1de5b747
SANJAY <sanjaypk986@gmail.com>
2024-10-10 17:16:55 +0530

    mogodb connected

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 export const connectDB = async()=>{
     try {
-        await mongoose.connect("mongodb+srv://sanjaypk986:<db_password>@cluster0.z20ti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.CONNECTION_STRING)
         console.log("Database connected");
         
     } catch (error) {
