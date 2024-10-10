f49bc83438ef2aaaf8b7a6c9fd30641acca8a356
SANJAY <sanjaypk986@gmail.com>
2024-10-10 19:50:32 +0530

    api checked using postman

--- routes/dashBoardRoutes.js
+++ routes/dashBoardRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { productCreate, UpdateQuantity } from '../controllers/dashBoardController.js'
+import { deleteProduct, productCreate, TotalProducts, TotalSalesAmount, UpdateQuantity } from '../controllers/dashBoardController.js'
 
 
 const router = express.Router()
 
+router.get('/all-products', TotalProducts)
+router.get('/total-amount' , TotalSalesAmount)
 router.post('/create',productCreate)
 router.patch('/update',UpdateQuantity)
+router.delete('/delete',deleteProduct)
 
 
 export default router
