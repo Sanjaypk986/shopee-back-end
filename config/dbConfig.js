52f796cac0f07f11eeb2a662730972c476b60ba3
swafvanSFK <safuswafvan1@gmail.com>
2024-10-11 09:22:42 +0530

    DB Check

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,7 +5,6 @@ export const connectDB = async()=>{
     try {
         await mongoose.connect(process.env.CONNECTION_STRING)
         console.log("Database connected");
-        
     } catch (error) {
         console.log(error);
         
